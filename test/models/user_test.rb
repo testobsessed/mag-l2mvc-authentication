c297ebd48fe12a704592614a9f7da6112f5d3eb6
Elisabeth Hendrickson <esh@qualitytree.com>
2023-02-12 11:32:45 -0800

    User model validates

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = User.new
+  end
+  test "Should require email" do
+    @user.password = "abc"
+    assert_not @user.save
+  end
+  test "Should require password" do
+    @user.email = "foo@bar.com"
+    assert_not @user.save
+  end
+  test "Should save with required params" do
+    @user.email = "foo@bar.com"
+    @user.password = "abc"
+    assert @user.save
+  end
 end
