7d3f875847a900276a2fc44dbea1dcfe44cdcffb
Elisabeth Hendrickson <esh@qualitytree.com>
2023-02-13 17:07:49 -0800

    Generated posts scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   get 'dashboards/index', as: :dashboards
   resources :logins, only: [:new, :create]
   resources :registrations, only: [:new, :create]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_12_183201) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_13_154057) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password"
@@ -18,4 +27,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_12_183201) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
