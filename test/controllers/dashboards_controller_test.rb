269930636fa74be13cceaf2ff271e2a4eff475d6
Elisabeth Hendrickson <esh@qualitytree.com>
2023-02-20 16:31:16 -0800

    Remove irretrievably broken tests

--- test/controllers/dashboards_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class DashboardsControllerTest < ActionDispatch::IntegrationTest
-  test "should redirect if not logged in" do
-    get dashboards_path
-    assert_response :redirect
-  end
-end
